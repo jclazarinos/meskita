85ca46fcdc0f2e8a2d73d71882e34997867ae115
jclazarinos <jean.lazarinos@tecsup.edu.pe>
2025-04-30 01:47:38 -0500

    coneccion base de datos en la nube mongoatlas

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const connectDB = require('./config/db');
 const usuarioRoutes = require('./routes/usuario.routes');
@@ -5,20 +7,25 @@ const platoRoutes = require('./routes/plato.routes');
 const pedidoRoutes = require('./routes/pedido.routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Conectar a MongoDB
 connectDB();
 
+// Middleware
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('✅ API del sistema de pedidos para restaurantes funcionando');
-  });
-  
 
+// Ruta base
+app.get('/', (req, res) => {
+  res.send('✅ API del sistema de pedidos para restaurantes funcionando');
+});
+
+// Rutas de la API
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/platos', platoRoutes);
 app.use('/api/pedidos', pedidoRoutes);
 
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/restaurante');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ Conectado a MongoDB');
   } catch (error) {
-    console.error('❌ Error de conexión:', error.message);
-    process.exit(1);
+    console.error('❌ Error al conectar a MongoDB:', error.message);
+    process.exit(1); // Finaliza el proceso si falla la conexión
   }
 };
 
